1de0fe34b774a9c12d5d350ced26135c64551b58
Mechergui Rami <street.cherk@gmail.com>
2023-01-27 02:03:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello, World!\n");
+});
+
+app.get("/mars", function (req, res) {
+  res.send("Hello Mars!\n");
+});
+
+app.listen(8080, function () {
+  console.log("Example app listening on port 8080!");
+});
+
+module.exports = app;
